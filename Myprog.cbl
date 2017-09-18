e24b1e413f7672973587657094486117277e14a5
Sam Niehaus <sen1019@gmail.com>
2017-09-18 08:39:54 -0400

    Initializing Commit

---
+++ Myprog.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:Sam Niehaus
+      * Date:18-09-17
+      * Purpose:Identification division practice
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Myprog.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM Myprog.
